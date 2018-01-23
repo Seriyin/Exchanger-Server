b7a8c983b081bb19d045d5abe6532f3d3c9eb658
Seriyin <redan.blue27@gmail.com>
2018-01-23 16:44:26 +0000

    Added an heatbeat outgoing task repeating on a delay on timer thread.

---
+++ src/main/java/app/OnlineTask.java
@@ -0,0 +1,37 @@
+package app;
+
+import com.google.protobuf.MessageLite;
+import org.zeromq.ZMQ;
+import proto.WrapperServer.WrapperMessageServer;
+import proto.Heartbeat.Online;
+
+import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * OnlineTask executes on a timer to alert connected exchange
+ * server of availability.
+ */
+public class OnlineTask extends TimerTask
+{
+    private final String name;
+    private final BlockingQueue<WrapperMessageServer> outgoing;
+
+    public OnlineTask(String name,
+                      BlockingQueue<WrapperMessageServer> outgoingQueue)
+    {
+        this.name = name;
+        outgoing = outgoingQueue;
+    }
+
+    public void run() {
+        WrapperMessageServer.Builder message = WrapperMessageServer.newBuilder();
+        message.setIsOnline(true);
+        Online.Builder builder = Online.newBuilder();
+        builder.setName(name);
+        builder.setStatus(200);
+        message.setOnline(builder);
+        outgoing.add(message.build());
+    }
+
+}
