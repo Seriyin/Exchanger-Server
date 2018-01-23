6c27166565d6d846e507aa6193816aca65b8e5f0
Seriyin <redan.blue27@gmail.com>
2018-01-23 17:44:26 +0000

    TradePeak is a JSONable Immutable POJO.

--- src/main/java/model/TradePeak.java
+++ src/main/java/model/TradePeak.java
@@ -8,9 +8,45 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class TradePeak
 {
+    private final double pps;
+    private final long time;
+    private final String name;
+    private final String exchange;
+
     @JsonCreator
-    public TradePeak(double pps, long time, String name, String exchange)
+    public TradePeak(@JsonProperty("pps") double pps,
+                     @JsonProperty("time") long time,
+                     @JsonProperty("name") String name,
+                     @JsonProperty("exchange") String exchange)
     {
 
+        this.pps = pps;
+        this.time = time;
+        this.name = name;
+        this.exchange = exchange;
+    }
+
+    @JsonProperty
+    public double getPps()
+    {
+        return pps;
+    }
+
+    @JsonProperty
+    public long getTime()
+    {
+        return time;
+    }
+
+    @JsonProperty
+    public String getName()
+    {
+        return name;
+    }
+
+    @JsonProperty
+    public String getExchange()
+    {
+        return exchange;
     }
 }
